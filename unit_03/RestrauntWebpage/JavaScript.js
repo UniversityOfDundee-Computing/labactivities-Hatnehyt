1497e44e31a119d49e4ded1c77525a1991b13774
Jack Henderson <24339931@dundee.ac.uk>
2022-10-22 17:25:00 +0100

    Restraunt Webpage Complete

---
+++ unit_03/RestrauntWebpage/JavaScript.js
@@ -0,0 +1,31 @@
+function SnacksShow(){
+    document.getElementById("SnacksDiv").style.display="block";
+    document.getElementById("DishDiv").style.display="none";
+    document.getElementById("BurgerDiv").style.display="none";
+    document.getElementById("DesertDiv").style.display="none";
+    document.getElementById("IntroText").style.display="none";
+    }
+    
+    function BurgerShow(){
+    document.getElementById("SnacksDiv").style.display="none";
+    document.getElementById("DishDiv").style.display="block";
+    document.getElementById("BurgerDiv").style.display="none";
+    document.getElementById("DesertDiv").style.display="none";
+    document.getElementById("IntroText").style.display="none";
+    }
+    
+    function DishesShow(){
+    document.getElementById("SnacksDiv").style.display="none";
+    document.getElementById("DishDiv").style.display="none";
+    document.getElementById("BurgerDiv").style.display="block";
+    document.getElementById("DesertDiv").style.display="none";
+    document.getElementById("IntroText").style.display="none";
+    }
+    
+    function DesertsShow(){
+    document.getElementById("SnacksDiv").style.display="none";
+    document.getElementById("DishDiv").style.display="none";
+    document.getElementById("BurgerDiv").style.display="none";
+    document.getElementById("DesertDiv").style.display="block";
+    document.getElementById("IntroText").style.display="none";
+    }
