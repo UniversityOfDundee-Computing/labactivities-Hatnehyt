5f1be9ac8d223949609e7ab0fa43d6440041ed74
Jack Henderson <24339931@dundee.ac.uk>
2022-11-17 17:47:29 +0000

    added

---
+++ unit_07/Activity 1/javascript.js
@@ -0,0 +1,33 @@
+function revert(){
+    document.getElementById("body").style.backgroundColor = "grey";
+    document.getElementById("h1tag").style.color = "black";
+}
+
+function blackSwitch(){
+    document.getElementById("body").style.backgroundColor = "black";
+    document.getElementById("h1tag").style.color = "white";
+}
+function redSwitch(){
+    document.getElementById("body").style.backgroundColor = "#ff7e79";
+}
+function orangeSwitch(){
+    document.getElementById("body").style.backgroundColor = "#ffd479";
+}
+function yellowSwitch(){
+    document.getElementById("body").style.backgroundColor = "#fffc79";
+}
+function lightgreenSwitch(){
+    document.getElementById("body").style.backgroundColor = "#d4fb78";
+}
+function darkgreenswitch(){
+    document.getElementById("body").style.backgroundColor = "#73fb79";
+}
+function cyanSwitch(){
+    document.getElementById("body").style.backgroundColor = "#73fcd5";
+}
+function lightblueSwitch(){
+    document.getElementById("body").style.backgroundColor = "#73fdff";
+}
+function darkblueSwitch(){
+    document.getElementById("body").style.backgroundColor = "#76d5ff";
+}
